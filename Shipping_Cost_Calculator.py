d0e1afb2af49b4a12cfe7525f3c9dc76bbed08c3
ashok <ashokrawaldz@gmail.com>
2025-10-07 11:38:05 -0400

    Revert "Revert "added additional line to Shipping_Cost_Calculator.py""
    This reverts commit 4757f73b500ccb76e28dfaf81e8513592d659d0a.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -13,3 +13,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by <your GitHub username>
+ # Here is another update by <your GitHub username>
